32e4bc3e90405fc91c2ad85cf173b1206a6cec2f
marto-k <martow17@gmail.com>
2016-04-30 19:40:26 +0300

    add files

---
+++ app/src/main/java/com/martin/m_radio/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.martin.m_radio;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static final int SPLASH_DELAY = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // start the radio service
+        Intent intent = new Intent(this, RadioService.class);
+        startService(intent);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DELAY);
+
+    }
+}
---
+++ app/src/main/java/com/martin/m_radio/schedule/ExoPlayer.java
@@ -0,0 +1,10 @@
+package com.martin.m_radio.schedule;
+
+/**
+ * Created by mkowusu on 10/4/15.
+ */
+public class ExoPlayer {
+
+    
+
+}
